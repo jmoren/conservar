06ca732fdc8d825fd51a39216ecc1144ffb4ed63
Jorge Moreno <jmoren@gmail.com>
2014-07-20 21:45:13 -0300

    changed assets vendor path

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,10 @@ module Conservar
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+    
+    config.assets.paths << Rails.root.join('vendor', 'assets')
     config.assets.paths << Rails.root.join('assets', 'fonts')
+
     config.generators do  |g|
       g.stylesheets     false
       g.javascripts     false
