9761197fac3654a2a4541a9c3fffc4b981eef0b7
Jorge Moreno <jmoren@gmail.com>
2014-08-08 21:54:12 -0300

    fixed typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {  
     :address              => "smtp.gmail.com",  
     :port                 => 587,  
-    :domain               => ENV["DOMAIN"]
+    :domain               => ENV["DOMAIN"],
     :user_name            => ENV["GMAIL_USER"],  
     :password             => ENV["GMAIL_PASSWORD"],  
     :authentication       => "plain"
