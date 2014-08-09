db1b23f782ba6e6764137d38daf71ba685a796d7
Jorge Moreno <jmoren@gmail.com>
2014-08-08 22:09:56 -0300

    Fixed smtp conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
     :address              => "smtp.gmail.com",  
     :port                 => 587,  
     :domain               => ENV["DOMAIN"],
-    :user_name            => ENV["GMAIL_USER"],  
+    :user_name            => ENV["GMAIL_USERNAME"],  
     :password             => ENV["GMAIL_PASSWORD"],  
     :authentication       => "plain",
     :enable_starttls_auto => true
